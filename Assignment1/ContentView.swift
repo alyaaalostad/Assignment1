8dce22ae1a58465473107bbbe8a3a5542e4ca4fa
Alyaa AlOstad <alyaaalostad@hotmail.com>
2020-05-02 23:20:55 +0300

    Initial Commit

---
+++ Assignment1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Assignment1
+//
+//  Created by Alyaa AlOstad on 5/2/20.
+//  Copyright © 2020 Alyaa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
